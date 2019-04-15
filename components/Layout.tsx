e9cf4b8abf6b196d6420f94c66e66297431c68e5
Jarrett Carraway <jarrett.s.carraway@gmail.com>
2019-04-15 00:32:36 -0400

    Update head id

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -24,9 +24,10 @@ const Layout: React.FunctionComponent<Props> = ({
       <script
         dangerouslySetInnerHTML={{
           __html: `window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=t.forceSSL||"https:"===document.location.protocol,a=document.createElement("script");a.type="text/javascript",a.async=!0,a.src=(r?"https:":"http:")+"//cdn.heapanalytics.com/js/heap-"+e+".js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(a,n);for(var o=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],c=0;c<p.length;c++)heap[p[c]]=o(p[c])};
-      heap.load("3073375350");`,
+          heap.load("1153379299");`,
         }}
       />
+      <script type="text/javascript" />
     </Head>
 
     <style jsx global>{`
